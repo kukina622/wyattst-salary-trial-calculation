5ab8ed9c637a7f4ea6f33da1b1a5845d5d9346ef
Min <wyes91622@gmail.com>
2024-05-09 03:32:31 +0800

    feat: 試算薪資(總工時)

--- index.js
+++ index.js
@@ -36,11 +36,31 @@ export default new Vue({
         breakTimeCalcResult: {
             dailyBreakTime: "",
         },
+        totalWorksHoursSalaryCalcCondition: {
+            govtMinWage: "",
+            monthWorkHours: "",
+            monthOvertimeHours: "",
+            monthFullTimeOverHours: [""],
+            workingDays: "",
+            containHoliday: false,
+        },
+        totalWorksHoursSalaryCalcResult: {
+            minimumWage: null,
+            overtimePay: null,
+            totalSalary: null,
+            hourlyWage: null,
+            overtimePay_NoFullTimeOvertime: null,
+            fullTimeOvertimePay: null,
+            dailyWage: null,
+            holidayPay: null,
+        }
     },
     computed: {
+        // 時薪
         hourlyWage() {
             return new Decimal(this.govtMinWage).div(8).div(30);
         },
+        // 最低基本工資(月)
         govtMinWage() {
             return this.computedCondition.govtMinWage?.replace(",", "") || 0;
         },
@@ -53,11 +73,13 @@ export default new Vue({
                     return this.salaryCalcCondition;
                 case 2:
                     return this.breakTimeCalcCondition;
+                case 3:
+                    return this.totalWorksHoursSalaryCalcCondition;
             }
         },
     },
     methods: {
-        onsalaryCalcSubmit() {
+        onSalaryCalcSubmit() {
             const govtMinWage = parseInt(this.govtMinWage) || 0;
             const holiday = this.holiday;
             const workingDays = parseInt(this.salaryCalcCondition.workingDays) || 0;
@@ -106,6 +128,34 @@ export default new Vue({
                     break;
                 }
             }
+        },
+        onTotalWorksHoursSalaryCalcSubmit() {
+            const govtMinWage = parseInt(this.govtMinWage) || 0;
+            const holiday = this.holiday;
+            const workingDays = parseInt(this.totalWorksHoursSalaryCalcCondition.workingDays) || 0;
+            const monthWorkHours = parseInt(this.totalWorksHoursSalaryCalcCondition.monthWorkHours) || 0;
+            const monthOvertimeHours = parseInt(this.totalWorksHoursSalaryCalcCondition.monthOvertimeHours) || 0;
+            const monthFullTimeOverHours = this.totalWorksHoursSalaryCalcCondition.monthFullTimeOverHours;
+
+            const {
+                minimumWage,
+                overtimePay_NoFullTimeOvertime,
+                fullTimeOvertimePay,
+                overtimePay,
+                totalSalary,
+                hourlyWage,
+                dailyWage,
+            } = this.salaryCalcByMonthWorkHours({govtMinWage, holiday, workingDays, monthWorkHours, monthOvertimeHours, monthFullTimeOverHours});
+
+            this.totalWorksHoursSalaryCalcResult = {
+                minimumWage,
+                overtimePay,
+                totalSalary,
+                hourlyWage,
+                overtimePay_NoFullTimeOvertime,
+                fullTimeOvertimePay,
+                dailyWage,
+            };
         },
         /**
          * @returns {Object}
@@ -190,21 +240,105 @@ export default new Vue({
             const dailyWage = totalSalary.div(workingDays);
 
             return {
-                minimumWage: resultMinimumWage,
-                overtimePay_NoFullTimeOvertime: overtimePay_NoFullTimeOvertime,
-                fullTimeOvertimePay: fullTimeOvertimePay,
-                hourlyWage: resultHourlyWage,
-                overtimePay,
-                totalSalary,
-                dailyWage,
+                minimumWage: resultMinimumWage.toDecimalPlaces(3),
+                overtimePay_NoFullTimeOvertime: overtimePay_NoFullTimeOvertime.toDecimalPlaces(3),
+                fullTimeOvertimePay: fullTimeOvertimePay.toDecimalPlaces(3),
+                hourlyWage: resultHourlyWage.toDecimalPlaces(3),
+                overtimePay: overtimePay.toDecimalPlaces(3),
+                totalSalary: totalSalary.toDecimalPlaces(3),
+                dailyWage: dailyWage.toDecimalPlaces(3),
             };
         },
+
+        /**
+         * @returns {Object}
+         * @property {Decimal} minimumWage 最低基本工資
+         * @property {Decimal} overtimePay_NoFullTimeOvertime 延長工時工資(不含全日加班)
+         * @property {Decimal} overtimePay 總延長工時工資
+         * @property {Decimal} totalSalary 總工資
+         * @property {Decimal} hourlyWage 時薪
+         * @property {Decimal} dailyWage 日薪
+         */
+        salaryCalcByMonthWorkHours({ govtMinWage, holiday, workingDays, monthWorkHours, monthOvertimeHours, monthFullTimeOverHours = [] }) {
+
+            monthWorkHours = 
+                monthWorkHours - 
+                monthOvertimeHours - 
+                monthFullTimeOverHours.reduce((acc, cur) => acc + (parseInt(cur) || 0), 0);
+
+            let normalMonthWorkHours = monthWorkHours > 240 ? 240 : monthWorkHours;
+            if (holiday > 0) {
+                normalMonthWorkHours += 10;
+            }
+
+            // 最低基本工資
+            let resultMinimumWage;
+
+            if (normalMonthWorkHours < 174) {
+                resultMinimumWage = new Decimal(
+                    240 - 174
+                )
+                    .times(this.hourlyWage)
+                    .add(govtMinWage)
+                    .div(24)
+                    .div(10)
+                    .times(normalMonthWorkHours);
+            } else {
+                resultMinimumWage = new Decimal(normalMonthWorkHours - 174)
+                    .times(this.hourlyWage)
+                    .add(govtMinWage);
+            }
+
+            // 延長工時時數(不含全日加班)
+            const overtimePay_NoFullTimeOvertime = new Decimal(monthOvertimeHours)
+                .times(4)
+                .div(3)
+                .times(this.hourlyWage);
+
+            // 全日加班天數
+            let fullTimeOvertimePay = new Decimal(0);
+            
+            for (const _hour of monthFullTimeOverHours) {
+                const hour = parseInt(_hour) || 0;
+                if (hour > 0) {
+                    const first = hour > 2 ? 2 : hour;
+                    const firstPay = new Decimal(first).times(4).div(3).times(this.hourlyWage);
+
+                    const second = hour - first;
+                    let secondPay = new Decimal(0);
+                    if (second > 0) {
+                        secondPay = new Decimal(second).times(5).div(3).times(this.hourlyWage);
+                    }
+                    fullTimeOvertimePay = fullTimeOvertimePay.add(firstPay).add(secondPay);
+                }
+            }
+
+            // 試算
+            const overtimePay = overtimePay_NoFullTimeOvertime.add(fullTimeOvertimePay);
+            const totalSalary = resultMinimumWage.add(overtimePay);
+            const dailyWage = totalSalary.div(workingDays);
+            const hourlyWage = totalSalary.div(monthWorkHours);
+
+            return {
+                minimumWage: resultMinimumWage.ceil(),
+                overtimePay_NoFullTimeOvertime: overtimePay_NoFullTimeOvertime.ceil(),
+                overtimePay: overtimePay.ceil(),
+                hourlyWage: hourlyWage.ceil(),
+                totalSalary: totalSalary.ceil(),
+                dailyWage: dailyWage.ceil(),
+                fullTimeOvertimePay: fullTimeOvertimePay.ceil(),
+            };
+        },
+        
         saveGovtMinWage() {
             localStorage.setItem("govtMinWage", this.govtMinWage);
         },
         getGovtMinWage() {
             return localStorage.getItem("govtMinWage") ?? "";
         },
+        addFullTimeOverHours() {
+            this.totalWorksHoursSalaryCalcCondition.monthFullTimeOverHours.push("");
+        }
     },
     mounted() {
         const govtMinWage = this.getGovtMinWage();
@@ -220,7 +354,14 @@ export default new Vue({
 
         let auto = new URLSearchParams(window.location.search).get('auto');
         if (auto) {
-            this.onsalaryCalcSubmit();
+            this.onSalaryCalcSubmit();
+        }
+    },
+    watch: {
+        page() {
+            this.$nextTick(() => {
+                $('[data-toggle="tooltip"]').tooltip();
+            });
         }
     }
 });
